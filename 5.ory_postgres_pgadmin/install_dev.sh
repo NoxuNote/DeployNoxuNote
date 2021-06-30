9d44396107ebd5d0a9f9bbad2df889fc6de062de
Léo <rolland.leo@orange.fr>
2021-06-30 15:45:26 +0200

    fix: missing id_token mutator

--- 5.ory_postgres_pgadmin/install_dev.sh
+++ 5.ory_postgres_pgadmin/install_dev.sh
@@ -1,3 +1,5 @@
-export BASE_URL="http://localhost"
-envsubst < values.yaml | helm install noxucloud -f - .
+echo "Do not forget to put your jwks.json file in this folder."
+export ORY_PROTOCOL="http"
+export ORY_HOST="127.0.0.1"
+envsubst < values.yaml | helm install noxucloud -f - --set-file 'oathkeeper.oathkeeper.mutatorIdTokenJWKs=./jwks.json' .
 

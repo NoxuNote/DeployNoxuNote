cdfcd77563f7d5ce87b1446e6ff0289d5a10f973
Léo <rolland.leo@orange.fr>
2021-07-12 18:02:08 +0200

    add: caching image pulls kind cluster

--- kindCluster/create_cluster.sh
+++ kindCluster/create_cluster.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -o errexit
 
-export HTTP_PROXY=http://172.19.0.40:3128
-export HTTPS_PROXY=$HTTP_PROXY
-export NO_PROXY="localhost, 127.0.0.*"
+# export HTTP_PROXY=http://172.19.0.40:3128
+# export HTTPS_PROXY=$HTTP_PROXY
+# export NO_PROXY="localhost, 127.0.0.*"
 
 # create cluster
 kind create cluster --config 3workers.yml

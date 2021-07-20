f54543e36e1e71f938c0dc2491c8aee4d61af142
Léo <rolland.leo@orange.fr>
2021-07-20 11:35:28 +0200

    add: oathkeeper & kratos scraping

--- redeploy_cluster.sh
+++ redeploy_cluster.sh
@@ -1,6 +1,7 @@
 cd kindCluster
 ./delete_cluster.sh
 ./create_cluster.sh
+./linkProxy.sh
 cd ..
 ./enable_nginx_ingress_controller.sh
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission

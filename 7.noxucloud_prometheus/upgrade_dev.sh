c5809998d1dd3b7483446aec108b2a9eb8d3795a
Léo <rolland.leo@orange.fr>
2021-07-22 12:05:13 +0200

    add: custom metrics in prometheus postgres exp

---
+++ 7.noxucloud_prometheus/upgrade_dev.sh
@@ -0,0 +1,5 @@
+echo "Do not forget to put your jwks.json file in this folder and set your /etc/hosts according to ingress HOSTS."
+export ORY_PROTOCOL="http"
+export ORY_HOST="localhost"
+envsubst < values.yaml | helm upgrade noxucloud -f - --set-file 'oathkeeper.oathkeeper.mutatorIdTokenJWKs=./jwks.json' .
+
